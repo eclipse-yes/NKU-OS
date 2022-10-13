dd0d81fa9a27b3d4165517ee7eff0db32a701a87
Shuhao Zhang <eclipse.tenet@gmail.com>
2022-10-13 15:42:28 +0800

    2022-10-13; change ./lab2/main.c

--- lab2/main.c
+++ lab2/main.c
@@ -1,6 +1,16 @@
-// NKU COS OS
+// NKUCOS: OS
 // Author: zsh
-#include "main.h"
+#include <dirent.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+
+#define ID_LENGTH 200
+#define NDEBUG
 
 char org_src_path[ID_LENGTH];
 char org_dst_path[ID_LENGTH];
@@ -9,12 +19,12 @@ void handleOptions(int argc, char* argv[]) {
     // No options.
     if (argc == 1) {
         fprintf(stderr, "usage: dircopy [-h] [--help] [-d dir_name] [--directory dir_name] ...\n");
-        exit(FAILURE);
+        exit(EXIT_FAILURE);
     }
     // -h, --help.
     if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
         printf("MAN INFO\n");
-        exit(SUCCESS);
+        exit(EXIT_SUCCESS);
     }
     // -d dir_name, --directory dir_name.
     if (!strcmp(argv[1], "-d") || !strcmp(argv[1], "--directory")) {
@@ -26,25 +36,25 @@ void handleOptions(int argc, char* argv[]) {
         return;
     }
     fprintf(stderr, "\x1b[31mError: inputing wrong options!!!\x1b[0m\n");
-    exit(FAILURE);
+    exit(EXIT_FAILURE);
 }
 
 int isCreated(const char* path) {
     if (access(path, F_OK) == 0 && opendir(path) != NULL)
-        return SUCCESS;
+        return EXIT_SUCCESS;
     else
-        return FAILURE;
+        return EXIT_FAILURE;
 }
 
 // Copy file from src to dst.
 void copyFile(char* dst_path, char* src_path) {
     FILE* fp_dst = fopen(dst_path, "w"); if (fp_dst == NULL) {
         fprintf(stderr, "\x1b[31mFAILURE: open dst_path: %s; errno = %d\n\x1b[0m", dst_path, errno);
-        exit(FAILURE);
+        exit(EXIT_FAILURE);
     }
     FILE* fp_src = fopen(src_path, "r"); if (fp_src == NULL) {
         fprintf(stderr, "\x1b[31mFAILURE: open src_path: %s; errno = %d\n\x1b[0m", src_path, errno);
-        exit(FAILURE);
+        exit(EXIT_FAILURE);
     }
 
     char buf = fgetc(fp_src);
@@ -58,8 +68,8 @@ void copyFile(char* dst_path, char* src_path) {
 #ifdef DEBUG
     printf("SUCCESS: copyFile([dst_path]%s, [src_path]%s); copy %d bytes.\n", dst_path, src_path, cnt);
 #endif
-    fclose(fp_dst);
-    fclose(fp_src);
+    fclose(fp_dst); fp_dst = NULL;
+    fclose(fp_src); fp_src = NULL;
 }
 
 // Copy directory from src to dst.
@@ -76,7 +86,7 @@ void copyDir(char* dst_path, char* src_path) {
 #endif
     } else {
         fprintf(stderr, "\x1b[31m[\n Error: failed to create directory!!!\n dst = %s; src = %s\n]\n\x1b[0m", dst_path, src_path);
-        exit(FAILURE);
+        exit(EXIT_FAILURE);
     }
 
     DIR* src = opendir(src_path);
@@ -86,10 +96,17 @@ void copyDir(char* dst_path, char* src_path) {
             continue;
         char new_dst_path[ID_LENGTH]; strcpy(new_dst_path, dst_path); strcat(new_dst_path, "/"); strcat(new_dst_path, cur_node->d_name);
         char new_src_path[ID_LENGTH]; strcpy(new_src_path, src_path); strcat(new_src_path, "/"); strcat(new_src_path, cur_node->d_name);
+        // Step1: copy regular files.
         if (cur_node->d_type == DT_DIR) {
             copyDir(new_dst_path, new_src_path);
         } else if (cur_node->d_type == DT_REG) {
             copyFile(new_dst_path, new_src_path);
+        } else if (cur_node->d_type == DT_LNK) {
+        // Step2: copy symbolic link files.
+          	// Use link function to copy symbolic link file.
+            int res = link(new_src_path, new_dst_path);
+            if (res != 0)
+                printf("FAILURE: link([oldpath]%s, [newpath]%s)\n", new_src_path, new_dst_path);
         } else {
             printf("\x1b[31m[\n Found: d_type = %d; d_name = %s\n new_dst_path = %s; new_src_path = %s ]\n\x1b[0m",
                     cur_node->d_type, cur_node->d_name, new_dst_path,
